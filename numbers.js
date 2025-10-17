96cff27bc40015b6ab0ce143e29e0904e199b50a
Hasnain-Wakeel <wakeelanjum112@gmail.com>
2025-10-18 01:51:15 +0500

    Generating Random Numbers , Rounding Off The Numbers with JS Pre-Defined Methods !!!

--- numbers.js
+++ numbers.js
@@ -1,5 +1,96 @@
-// Generating Random Numbers between 0 and 1 :
+// ----------------- Generating Random Numbers between 0 and 1 : ----------------
 
-const random = Math.random();
-console.log(random);
+// const random = Math.random();
+// console.log(random);
+
+// let randomNumber = Math.random() * 10            // Multiplies every number by 10 ....
+// console.log(randomNumber)
+
+// -------------- Rounding Numbers : ----------------
+
+// let randomNumber = Math.round(0.12345)           // 0.12345 ~ 0
+// console.log(randomNumber)
+
+// let randomNumber = Math.round(4.786)             // 4.786 ~ 5
+// console.log(randomNumber)
+
+// let randomNumber = Math.round(21.435)            // 21.435 ~ 21
+// console.log(randomNumber)
+
+// let randomNumber = Math.round(21.543)            // 21.543 ~ 22
+// console.log(randomNumber)
+
+// let randomNumber = Math.round(-1.5)              // -1.5 ~ -1
+// console.log(randomNumber)
+
+// let randomNumber = Math.round(-1.51)             // -1.51 ~ -2
+// console.log(randomNumber)
+
+// let randomNumber = Math.round(-1.59)             // -1.59 ~ -2
+// console.log(randomNumber)
+
+// let randomNumber = Math.round(-12.45)            // -12.45 ~ -12
+// console.log(randomNumber)
+
+// let randomNumber = Math.round(-12.61)            // -12.61 ~ -13
+// console.log(randomNumber)
+
+
+
+// --------------- Using "Ceil" Instead of "Round" : ----------------
+
+// ---------- " Ceil Function rounds the number to the nearest larger integer : " -----------
+
+// ---------- " Ceil ka method number ko qareebi baray number/integer tak lay jaata hai" ---------
+
+
+// let number = Math.ceil(0.1)                   // 1
+// let number = Math.ceil(0.00001);              // 1
+// let number = Math.ceil(0.000011111);          // 1
+// console.log(number)
+
+// let number = Math.ceil(-0.0001);              // 0
+// console.log(number)
+
+// let number = Math.ceil(1.00001);              // 2
+// console.log(number);
+
+
+
+// --------------------- Using "Floor" Instead of Round : -----------------
+
+// ---------------- " Floor Function rounds the number to the nearest larger integer : " -----------
+
+// ----------- Floor ka method number ko qareebi chotay number ki taraf lay jaata hai : ------------
+
+
+// let number = Math.floor(1.999)                    // 1.999 ~ 1
+// console.log(number)
+
+// let number = Math.floor(0.1234)                   // 0.1234 ~ 0
+// console.log(number)
+
+// let number = Math.floor(-0.0001)                  // -0.0001 ~ -1
+// console.log(number)
+
+// let number = Math.floor(-1.999)                   // -1.999 ~ -2
+// console.log(number)
+
+// --------------------------------------------------------------------------------------------------------
+
+
+// ------------------ Generating Random Numbers From 1 To 6 : --------------------
+
+// let number = Math.ceil(Math.random() * 6)
+// console.log(number)
+
+
+// ------------------ Generating Random Numbers From 0 to 6 : --------------------
+
+// let number = Math.round(Math.random() * 6)
+// console.log(number)
+
+// let number = Math.floor(Math.random() * 7)
+// console.log(number)
+
 
